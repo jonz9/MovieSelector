94a53bbc1a671d7ddfc70caee6521c877b864068
John Zhang <zhangjohn.ca@gmail.com>
2023-04-11 23:03:18 -0400

    missed files from previous commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const APIkey = `e552c8258b3f71b20f3fc069ca964a73`;
 
-getbutton.addEventListener("getMovie", async () => {
-  let movieValue = movies.children("option:selected").val();
-  await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${APIkey}`);
+getbutton.addEventListener("click", async () => {
+  let movieSelected = document.getElementById("movies");
+  let movieValue = movieSelected.options[movieSelected.selectedIndex];
+  // await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${APIkey}`);
   console.log(movieValue);
 });
