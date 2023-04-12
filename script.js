d5bb4adfedb42949d1ef29263760e50a5c883ed4
John Zhang <zhangjohn.ca@gmail.com>
2023-04-11 23:28:54 -0400

    fixed confusing variable names

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const APIkey = `e552c8258b3f71b20f3fc069ca964a73`;
 
 getbutton.addEventListener("click", async () => {
-  let movieSelected = document.getElementById("movies");
-  let movieValue = movieSelected.options[movieSelected.selectedIndex];
-  // await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${APIkey}`);
-  console.log(movieValue);
+  let movies = document.getElementById("select");
+  let movieSelected = axios.get(`https://api.themoviedb.org/3/movie/${movies.options[movies.selectedIndex].value}?api_key=${APIkey}&language=en-US`, {
+    params: {
+      api_key: APIkey,
+      append_to_response: "videos",
+    },
+  })
+  movieSelected = movieSelected.then(() => {
+    console.log(movieSelected);
+  })
 });
