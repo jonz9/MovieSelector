0a35eeac91de8f7d6a9a7ae15deae0124e5042c1
John Zhang <zhangjohn.ca@gmail.com>
2023-04-11 22:51:46 -0400

    created movie info section, button, and EventListener for get button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-import axios from "axios";
+const APIkey = `e552c8258b3f71b20f3fc069ca964a73`;
 
-const keyAPI = 'e552c8258b3f71b20f3fc069ca964a73';
-const URL = 'https://api.themoviedb.org/3/movie/76341?api_key=${keyAPI}';
-const data = axios.get(URL);
-
+getbutton.addEventListener("getMovie", async () => {
+  let movieValue = movies.children("option:selected").val();
+  await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${APIkey}`);
+  console.log(movieValue);
+});
