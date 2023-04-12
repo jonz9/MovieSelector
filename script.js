3467f56b46569bc02223b83096d9579b0ddd18d7
John Zhang <zhangjohn.ca@gmail.com>
2023-04-12 13:55:18 -0400

    created variables for grabbing elements from DOM to manipulate

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ getbutton.addEventListener("click", async () => {
       append_to_response: "videos",
     },
   })
-  movieSelected = movieSelected.then(() => {
+  .then((movieSelected) => {
+    const title = document.getElementById("title");
+    const poster = document.getElementById("poster");
+    const genres = document.getElementById("genres");
+    const popularity = document.getElementById("popularity");
+    const releaseDate = document.getElementById("release-date");
+    const averageScore = document.getElementById("average-score");
+    const overview = document.getElementById("overview");
+    const trailers = 
     console.log(movieSelected);
+    
+  })
+  .catch(() => {
+    console.log("error");
   })
 });
