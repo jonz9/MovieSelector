604a747412ec55c84cec91ea77b2ce68611525ab
John Zhang <zhangjohn.ca@gmail.com>
2023-04-13 00:58:09 -0400

    changed background, fonts, and added manipulation to DOM through js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const APIkey = `e552c8258b3f71b20f3fc069ca964a73`;
+const getTMDBData = async (url) => {
+  return (await axios.get(url)).data;
+}
 
 getbutton.addEventListener("click", async () => {
   let movies = document.getElementById("select");
-  let movieSelected = axios.get(`https://api.themoviedb.org/3/movie/${movies.options[movies.selectedIndex].value}?api_key=${APIkey}&language=en-US`, {
-    params: {
-      api_key: APIkey,
-      append_to_response: "videos",
-    },
-  })
-  .then((movieSelected) => {
-    const title = document.getElementById("title");
-    const poster = document.getElementById("poster");
-    const genres = document.getElementById("genres");
-    const popularity = document.getElementById("popularity");
-    const releaseDate = document.getElementById("release-date");
-    const averageScore = document.getElementById("average-score");
-    const overview = document.getElementById("overview");
-    const trailers = 
-    console.log(movieSelected);
+  let movieSelected = await getTMDBData(`https://api.themoviedb.org/3/movie/${movies.options[movies.selectedIndex].value}?api_key=${APIkey}&language=en-US`)
+  .then(async (movieSelected) => {
     
-  })
-  .catch(() => {
-    console.log("error");
-  })
+    console.log(movieSelected)
+    let movieSection = document.getElementById("movie-section");
+    let container = document.getElementById("container")
+    document.body.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movieSelected.backdrop_path})`
+    document.body.style.backgroundRepeat = `no-repeat`;
+    document.body.style.backgroundSize = `cover`;
+    movieSection.style.backgroundColor = `black`;
+
+    document.getElementById("title").innerHTML = movieSelected.title;
+    document.getElementById("poster").src = `https://image.tmdb.org/t/p/w500${movieSelected.poster_path}`;
+    document.getElementById("average-score").innerHTML = `Average Score: ${movieSelected.vote_average}`;
+    document.getElementById("popularity").innerHTML = `Popularity: ${movieSelected.vote_count}`;
+    document.getElementById("runtime").innerHTML = `Runtime: ${movieSelected.runtime}`;
+    document.getElementById("release-date").innerHTML = `Release Date: ${movieSelected.release_date}`;
+    document.getElementById("genres").innerHTML = `Genre: ${movieSelected.genres[0].name}`;
+    document.getElementById("overview").innerHTML = `Synopsis: ${movieSelected.overview}`;
+
+    const trailer = await getTMDBData(`https://api.themoviedb.org/3//movie/${movieSelected.value}/videos?api_key=${APIkey}&language=en-US&adult=false`).results.filter((trailer) => trailer.type === "Trailer");
+    document.getElementById("trailer").src = `https://www.youtube.com/watch?v=${trailer.at(0).key}`;
+  });
 });
